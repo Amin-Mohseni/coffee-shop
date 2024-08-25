c6d2f69d9a2154ae27fb9f59a70cca718a12fc29
Amin Mohseni <am0101010101in@gmail.com>
2024-08-25 13:04:21 +0330

    add api for products

---
+++ app/api/products/route.tsx
@@ -0,0 +1,53 @@
+import connectToDB from "@/configs/db";
+import productModel from "@/models/Product";
+
+type RequestBody = {
+  name: String;
+  price: Number;
+  shortDescription: String;
+  longDescription: String;
+  weight: Number;
+  suitableFor: String;
+  smell: String;
+  tags: [String];
+};
+
+export async function POST(req: Request, res: Response) {
+  try {
+    await connectToDB();
+    const body = await req.json();
+    const {
+      name,
+      price,
+      shortDescription,
+      longDescription,
+      weight,
+      suitableFor,
+      smell,
+      tags,
+    } = body as RequestBody;
+
+    const product = await productModel.create({
+      name,
+      price,
+      shortDescription,
+      longDescription,
+      weight,
+      suitableFor,
+      smell,
+      tags,
+    });
+
+    return Response.json(
+      { message: "Product created successfully :", data: product },
+      { status: 201 }
+    );
+  } catch (error) {
+    return Response.json({ message: error }, { status: 500 });
+  }
+}
+
+export async function GET(req: Request, res: Response) {
+  const products = await productModel.find({}, "-__v").populate("comments");
+  return Response.json(products);
+}
