da9533f425de12adb82fbbf11b95d33afa6ed48c
Amin Mohseni <am0101010101in@gmail.com>
2024-08-30 18:49:33 +0330

    update products type

--- utils/types.tsx
+++ utils/types.tsx
@@ -5,9 +5,10 @@ export interface Comment {
 export type productType = {
   name: string;
   score: number;
+  smell: string;
   comments: Comment[];
   price: number;
   shortDescription: string;
   tags: [string];
-  _id : string
+  _id: string;
 };
